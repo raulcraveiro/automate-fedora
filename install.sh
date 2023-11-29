32d2b9aa8b660e9a21c37e835f668972f535400d
Raul Craveiro <raulcraveiro@gmail.com>
2023-11-29 17:13:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# Instalar o RPM Fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Instalar aplicativos em flatpak
+flatpak install flathub org.yuzu_emu.yuzu
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.spotify.Client
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub org.videolan.VLC
+flatpak install flathub org.gnome.Boxes
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub net.davidotek.pupgui2
+flatpak install flathub com.usebottles.bottles
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop
